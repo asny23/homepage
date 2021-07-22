20253c6b09405b5bc11d5cd82fe0e6a49aac8060
asny23 <asny23+github@gmail.com>
2021-07-22 23:31:08 +0900

    add produits page

---
+++ pages/produits/index.tsx
@@ -0,0 +1,12 @@
+import HeadTag from '../../components/head'
+
+export default function Produits() {
+  return (
+    <>
+      <HeadTag
+        title='Produits / asny23のホームページ'
+        description='made from thinking'
+      />
+    </>
+  )
+}
