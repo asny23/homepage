fdd4cd18b658daecd80a0355465c84fd4f472d20
asny23 <asny23+github@gmail.com>
2021-08-07 23:38:07 +0900

    netlify rewrite settings

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-  poweredByHeader: false,
-  async rewrites() {
-    return [
-      {
-        source: '/place/',
-        destination: 'https://place-asny23hub.vercel.app/place/'
-      },
-      {
-        source: '/place/:path*',
-        destination: 'https://place-asny23hub.vercel.app/place/:path*'
-      }
-    ]
-  }
+  poweredByHeader: false
 }
