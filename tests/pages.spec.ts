b3abd3115f9e49de50d0436497cbb0dd07b6748f
asny23 <asny23+github@gmail.com>
2023-04-09 16:00:34 +0900

    add produits page monitoring

--- tests/pages.spec.ts
+++ tests/pages.spec.ts
@@ -7,3 +7,9 @@ test('Top page', async ({ page }) => {
   expect(response.status()).toEqual(200)
   await expect(page.locator('h1')).toContainText('thinking cola')
 })
+
+test('Produits page', async ({ page }) => {
+  const response = await page.goto(siteUrl + 'produits')
+  expect(response.status()).toEqual(200)
+  await expect(page.locator('h1')).toContainText('thinking cola')
+})
